a872efeef67356265ef2a9f74e5fadee1c47b197
Denis-Wanjohi <deniswanjohi15@gmail.com>
2025-06-13 11:22:14 +0300

    Product project

--- JS/Day 4/script.js
+++ JS/Day 4/script.js
@@ -1,32 +1,24 @@
-// O B J E C T S
-// CREATING
-
-
-// ACCESSING ITEMS
-
-
-// DELETING ITEMS
-
-
-// NESTED OBJECTS
-
-
-// L O O P S
-// FOR LOOP 
-
-
-
-// FOR IN LOOP
-
-
-// WHILE LOOP
-
-
-
-// DO WHILE LOOP
-
-
-// BREAK 
-
-
-// CONTINUE
+fetch('https://dummyjson.com/products')
+.then(res => res.json())
+.then(res=>{
+    let products = res.products
+    products.forEach(element=>{
+        console.log(element.images[0])
+        let p_title  = document.createElement("p")
+        p_title.style.fontWeight = "bold"
+        let title = document.createTextNode(element.title)
+        let p_desc = document.createElement("p")
+        let desc = document.createTextNode(element.description)
+        let image = document.createElement("img")
+        image.src = element.images[0]
+        image.style.width = "100px"
+        image.style.height = "100px"
+        image.style.borderRadius = "50%"
+        p_title.appendChild(title)
+        p_desc.appendChild(desc)
+        document.getElementById("data").appendChild(image)
+        document.getElementById("data").appendChild(p_title)
+        document.getElementById("data").appendChild(p_desc)
+        
+    })
+});
